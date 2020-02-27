04595f4b80e22542263e24cc131420f1571c3785
alekslanin <alex_lanin@yahoo.com>
2020-02-26 19:18:39 -0500

    project started

---
+++ src/main/java/com/mc/citicraft/CiticraftApplication.java
@@ -0,0 +1,13 @@
+package com.mc.citicraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiticraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiticraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/citicraft/CiticraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.citicraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiticraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
